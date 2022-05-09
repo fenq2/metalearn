be764d0ca969e337b86fd3bdb67880ffd50d660e
fenq <fenzqr@gmail.com>
2022-05-09 17:05:32 +0300

    some fix

--- components/WriteForm/WriteForm.tsx
+++ components/WriteForm/WriteForm.tsx
@@ -1,16 +1,19 @@
-import styles from './WriteForm.module.scss';
 import dynamic from 'next/dynamic';
 import React from 'react';
 
-const Editor = dynamic(() => import('../Editor/Editor').then((m) => m.Editor), { ssr: false });
+import styles from './WriteForm.module.scss';
+
+const Editor = dynamic(() => import('../Editor/Editor').then((m) => m.Editor), {
+  ssr: false,
+});
 
 const WriteForm: React.FC = () => {
-	return (
-		<div className={styles.writeForm}>
-			<input type='text' placeholder='Заголовок' />
-			<Editor />
-		</div>
-	);
+  return (
+    <div className={styles.writeForm}>
+      <input type="text" placeholder="Заголовок" />
+      {/* <Editor /> */}
+    </div>
+  );
 };
 
 export { WriteForm };
